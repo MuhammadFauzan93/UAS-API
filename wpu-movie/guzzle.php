d948a088f1176c554ce3de58bb4d41792fa2e625
MuhammadFauzan93 <muhammadfauzan1412@hmail.com>
2025-06-01 22:09:38 +0700

    Update nambah guzzle

---
+++ wpu-movie/guzzle.php
@@ -0,0 +1,39 @@
+<?php 
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+
+$response = $client->request('GET','http://omdbapi.com',[ 
+    'query' => [ 
+        'apikey' => 'ffd4cfe4',
+        's' => 'transformers'
+    ]
+]);
+
+$result = json_decode ($response->getBody()->getContents(),true);
+
+var_dump($result);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie</title>
+</head>
+<body>
+    <?php foreach($result['Search'] as $movie) : ?>
+    <ul>
+        <li>Title : <?= $movie['Title']; ?></li>
+        <li>Year : <?= $movie['Year']; ?></li>
+        <li>
+            <img src="<?= $movie['Poster']; ?>" widht="80">
+        </li>
+    </ul>
+    <?php endforeach; ?>
+</body>
+</html>
